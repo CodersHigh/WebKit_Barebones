72348c690f0152047ccb2bc99bce6a2b93a3a9bd
이로운 <lru040409@gmail.com>
2022-08-01 15:54:49 +0900

    웹 주소 검색 구현

--- WebKit_Barebones/View/ContentView.swift
+++ WebKit_Barebones/View/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var viewModel = WebViewStateModel()
-    @State private var showWebView = false
-    private let urlString: String = "https://www.google.com"
+    @State private var webAddress = "https://google.com"
     
     var body: some View {
-        WebView(webViewStateModel: viewModel)
-            .cornerRadius(15)
-            .shadow(color: .black.opacity(0.2), radius: 5.0, x: 0, y: 0)
+        VStack {
+            HStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("웹 주소를 입력하세요.", text: $webAddress)
+                        .foregroundColor(.primary)
+                }
+                .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                .foregroundColor(.secondary)
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(15)
+                Button("검색") {
+                    viewModel.pageURLString = webAddress
+                    viewModel.goToPage = true
+                }
+            }
             .padding()
+            WebView(webViewStateModel: viewModel)
+                .cornerRadius(15)
+                .shadow(color: .black.opacity(0.2), radius: 5.0, x: 0, y: 0)
+                .padding()
+        }
     }
     
 }
