3f442ddecfb98079dce8e823d25d41d8b17c0f91
이로운 <lru040409@gmail.com>
2022-08-01 15:04:43 +0900

    WebViewStateModel 생성 및 WebView 수정

--- WebKit_Barebones/View/WebView.swift
+++ WebKit_Barebones/View/WebView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    
-    var url: URL
+    @ObservedObject var webViewStateModel: WebViewStateModel
     
     func makeUIView(context: Context) -> WKWebView {
+        let view = WKWebView(frame: CGRect.zero)
+        view.allowsBackForwardNavigationGestures = true
+        view.scrollView.isScrollEnabled = true
         return WKWebView()
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        uiView.load(request)
+        uiView.load(URLRequest(url: URL(string: webViewStateModel.pageURLString)!))
+        
+        if webViewStateModel.goToPage {
+            uiView.load(URLRequest(url: URL(string: webViewStateModel.pageURLString)!))
+            webViewStateModel.goToPage = false 
+        }
     }
     
 }
---
+++ WebKit_Barebones/ViewModel/WebViewStateModel.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewStateModel.swift
+//  WebKit_Barebones
+//
+//  Created by 이로운 on 2022/08/01.
+//
+
+import Foundation
+
+class WebViewStateModel: ObservableObject {
+    @Published var pageURLString: String = "https://goole.com"
+    @Published var goToPage: Bool = false
+    
+    @Published var goBack: Bool = false
+    @Published var goForward: Bool = false
+}
