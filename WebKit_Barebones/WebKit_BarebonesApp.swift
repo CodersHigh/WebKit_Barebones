57961b151b2fdb1d507f9978645ba4eefad3e365
이로운 <lru040409@gmail.com>
2022-08-01 14:12:56 +0900

    프로젝트 셋업

---
+++ WebKit_Barebones/WebKit_BarebonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebKit_BarebonesApp.swift
+//  WebKit_Barebones
+//
+//  Created by 이로운 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct WebKit_BarebonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
